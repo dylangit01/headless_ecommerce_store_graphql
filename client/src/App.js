d5f9111fe8acf480ac33e18f72c0768b75d2b0f1
Dylan <dcren2010@gmail.com>
2021-08-14 17:04:03 -0400

    downgrade node version so sass can be installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './default.scss'
 
 const App = () => {
   return (
---
+++ client/src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.scss'
+
+const index = () => {
+	return (
+		<header className='header'>
+			
+		</header>
+	)
+}
+
+export default index
