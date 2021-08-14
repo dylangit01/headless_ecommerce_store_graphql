2dc7a20679692c7c8750cc218520504d21de617a
Dylan <dcren2010@gmail.com>
2021-08-14 16:23:59 -0400

    update new project

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      App
+    <div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Signin from './Components/Signin';
-import Signup from './Components/Signup';
-import Checkout from './Components/Checkout';
 
-const Root = () => {
-  return (
-    <Router>
-    <Switch>
-      <Route component={App} exact path='/' />
-      <Route component={Signin} exact path='/signin' />
-      <Route component={Signup} exact path='/signup' />
-      <Route component={Checkout} exact path='/checkout' />
-    </Switch>
-  </Router>
-  )
-};
-
-ReactDOM.render( <Root /> , document.getElementById('root') );
+ReactDOM.render( <App /> , document.getElementById('root') );
