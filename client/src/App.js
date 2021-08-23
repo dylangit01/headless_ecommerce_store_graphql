9a56fb6c1bccc46c485d00a72143289bcc8ab541
Dylan <dcren2010@gmail.com>
2021-08-22 22:48:06 -0400

    add default scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import './default.scss'
+import './default.scss';
+import Header from './components/Header'
 
 const App = () => {
   return (
     <div>
+      <Header />
+      <h1>HEllo world!!!</h1>
       
     </div>
   )
